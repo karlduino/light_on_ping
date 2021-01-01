30baf61f89ac126c9bcf2df3828354fd33d6eea5
Karlduino <karlduino@gmail.com>
2020-12-31 21:55:04 -0600

    Revise for my old raspberry pi
     - include start-up lights to show that all of the lights are working

--- light_on_ping.py
+++ light_on_ping.py
@@ -5,14 +5,25 @@ import platform   # for getting operating system name
 import subprocess # for executing a shell command
 from time import sleep
 
-green1 = 20
-red1   = 21
+# wait time between pings
+ping_wait = 60
+startup_wait = 0.5
 
-green2 = 25
-red2   = 12
+# hosts
+host1 = '192.168.0.1'
+host2 = '192.168.0.2'
+host3 = '192.168.0.3'
 
-green3 = 23
-red3   = 24
+# LED pins
+green1 = 25    # (pin 22 on old raspberry pi with 13x2 layout)
+red1   = 24    # (pin 18)
+
+green2 = 23    # (pin 16)
+red2   = 22    # (pin 15)
+
+green3 = 27    # (pin 13)
+red3   =  4    # (pin 7)
+
 
 def ping(host):
     """
@@ -35,17 +46,26 @@ def light_on_ping(host, green_led, red_led):
         green_led.off()
         red_led.on()
 
-host1 = '192.168.0.1'
+def startup(leds, wait):
+    for led in leds:
+        led.on()
+        sleep(wait)
+    sleep(wait)
+    for led in leds:
+        led.off()
+        sleep(wait)
+
 led_green1 = LED(green1)
 led_red1 = LED(red1)
 
-host2 = '192.168.0.2'
 led_green2 = LED(green2)
 led_red2 = LED(red2)
 
-host3 = '192.168.0.3'
 led_green3 = LED(green3)
 led_red3 = LED(red3)
+
+startup([led_green1, led_green2, led_green3,
+         led_red1, led_red2, led_red3], startup_wait)
 
 while True:
     light_on_ping(host1, led_green1, led_red1)
